63e754cbb3e515391bff99d1646ee7b3310b4f4b
alextanhongpin <alextan220990@gmail.com>
2023-06-09 23:39:46 +0800

    chore: update tests using containers from core package

--- buntx/buntx_test.go
+++ buntx/buntx_test.go
@@ -4,32 +4,25 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
-	"log"
 	"os"
 	"testing"
-	"time"
 
+	"github.com/alextanhongpin/core/test/containers"
 	"github.com/alextanhongpin/dbtx/buntx"
 	_ "github.com/lib/pq"
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/extra/bundebug"
 )
 
-var bunDB *bun.DB
+const postgresVersion = "15.1-alpine"
 
 func TestMain(m *testing.M) {
-	stop := startDB()
+	stop := containers.StartPostgres(postgresVersion, migrate)
 	code := m.Run()
 	stop() // os.Exit does not care about defer.
 	os.Exit(code)
 }
 
 func TestBun(t *testing.T) {
+	bunDB := containers.PostgresBunDB(t)
 	// Setup unit of work.
 	u := buntx.New(bunDB)
 	ctx := context.Background()
@@ -76,85 +69,4 @@ func migrate(db *sql.DB) error {
 	unique(name)
 )`)
 	return err
-}
-
-func startDB() func() {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatal("could not construct pool:", err)
-	}
-
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatal("could not connect to docker:", err)
-	}
-
-	// Pulls an image, creates a container based on it and run it.
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        "15.1-alpine",
-		Env: []string{
-			"POSTGRES_PASSWORD=123456",
-			"POSTGRES_USER=john",
-			"POSTGRES_DB=test",
-			"listen_addresses = '*'",
-		},
-	}, func(config *docker.HostConfig) {
-		// Set AutoRemove to true so that stopped container goes away by itself.
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-	})
-	if err != nil {
-		log.Fatal("could not start resources:", err)
-	}
-	hostAndPort := resource.GetHostPort("5432/tcp")
-	databaseURL := fmt.Sprintf("postgres://john:123456@%s/test?sslmode=disable", hostAndPort)
-
-	log.Println("connecting to database on url:", databaseURL)
-
-	resource.Expire(120) // Tell docker to kill the container in 120 seconds.
-
-	// Exponential backoff-retry, because the application in the container might
-	// not be ready to accept connections yet.
-	pool.MaxWait = 120 * time.Second
-	if err = pool.Retry(func() error {
-		db, err := sql.Open("postgres", databaseURL)
-		if err != nil {
-			return err
-		}
-		if err := db.Ping(); err != nil {
-			return err
-		}
-		if err := migrate(db); err != nil {
-			return err
-		}
-
-		sqlDB := sql.OpenDB(pgdriver.NewConnector(
-			pgdriver.WithUser("john"),
-			pgdriver.WithAddr(hostAndPort),
-			pgdriver.WithPassword("123456"),
-			pgdriver.WithDatabase("test"),
-			pgdriver.WithApplicationName("test_app"),
-			pgdriver.WithInsecure(true),
-		))
-
-		bunDB = bun.NewDB(sqlDB, pgdialect.New())
-		bunDB.AddQueryHook(bundebug.NewQueryHook(
-			bundebug.WithVerbose(true),
-		))
-
-		return bunDB.Ping()
-	}); err != nil {
-		log.Fatal("could not connect to docker:", err)
-	}
-
-	return func() {
-		if err := bunDB.Close(); err != nil {
-			log.Println("failed to close bun:", err)
-		}
-
-		if err := pool.Purge(resource); err != nil {
-			log.Fatal("could not purge resource:", err)
-		}
-	}
 }
--- dbtx_test.go
+++ dbtx_test.go
@@ -4,39 +4,34 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"fmt"
-	"log"
 	"math/big"
 	"os"
 	"sync"
 	"testing"
 	"time"
 
+	"github.com/alextanhongpin/core/test/containers"
 	"github.com/alextanhongpin/dbtx"
 	"github.com/alextanhongpin/dbtx/postgres/lock"
 	_ "github.com/lib/pq"
-	"github.com/ory/dockertest"
-	"github.com/ory/dockertest/docker"
 )
 
-var db *sql.DB
+const postgresVersion = "15.1-alpine"
 
 var ErrIntentional = errors.New("intentional error")
 
 func TestMain(m *testing.M) {
-	stopDB := startDB()
-	migrate()
-
+	stop := containers.StartPostgres(postgresVersion, migrate)
 	// Run tests.
 	code := m.Run()
-
-	stopDB()
+	stop()
 
 	os.Exit(code)
 }
 
 func TestSQL(t *testing.T) {
 	var n int
+	db := containers.PostgresDB(t)
 	err := db.QueryRow("select 1 + 1").Scan(&n)
 	if err != nil {
 		t.Error(err)
@@ -45,6 +40,7 @@ func TestSQL(t *testing.T) {
 }
 
 func TestAtomic(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	err := tx.RunInTx(context.Background(), func(ctx context.Context) error {
 		tx := tx.DB(ctx)
@@ -78,6 +74,7 @@ func TestAtomic(t *testing.T) {
 }
 
 func TestAtomicNested(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	err := tx.RunInTx(context.Background(), func(ctx1 context.Context) error {
 		return tx.RunInTx(ctx1, func(ctx2 context.Context) error {
@@ -113,6 +110,7 @@ func TestAtomicNested(t *testing.T) {
 }
 
 func TestAtomicIntLockKey(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	err := tx.RunInTx(context.Background(), func(ctx context.Context) error {
 		return lock.Lock(ctx, lock.IntKey(1, 2))
@@ -123,6 +121,7 @@ func TestAtomicIntLockKey(t *testing.T) {
 }
 
 func TestAtomicIntLockKeyLocked(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	err := tx.RunInTx(context.Background(), func(txCtx context.Context) error {
 		locked1, err := lock.TryLock(txCtx, lock.IntKey(1, 1))
@@ -144,6 +143,7 @@ func TestAtomicIntLockKeyLocked(t *testing.T) {
 }
 
 func TestAtomicBigIntLockKey(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	err := tx.RunInTx(context.Background(), func(ctx context.Context) error {
 		return lock.Lock(ctx, lock.BigIntKey(big.NewInt(10)))
@@ -154,6 +154,7 @@ func TestAtomicBigIntLockKey(t *testing.T) {
 }
 
 func TestAtomicBigIntLockKeyLocked(t *testing.T) {
+	db := containers.PostgresDB(t)
 	tx := dbtx.New(db)
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -201,65 +202,7 @@ func TestAtomicBigIntLockKeyLocked(t *testing.T) {
 	wg.Wait()
 }
 
-func migrate() {
+func migrate(db *sql.DB) error {
 	_, err := db.Exec(`create table numbers(n int);`)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func startDB() func() {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatal("could not construct pool:", err)
-	}
-
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatal("could not connect to docker:", err)
-	}
-
-	// Pulls an image, creates a container based on it and run it.
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        "15.1-alpine",
-		Env: []string{
-			"POSTGRES_PASSWORD=123456",
-			"POSTGRES_USER=john",
-			"POSTGRES_DB=test",
-			"listen_addresses = '*'",
-		},
-	}, func(config *docker.HostConfig) {
-		// Set AutoRemove to true so that stopped container goes away by itself.
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-	})
-	if err != nil {
-		log.Fatal("could not start resources:", err)
-	}
-	hostAndPort := resource.GetHostPort("5432/tcp")
-	databaseURL := fmt.Sprintf("postgres://john:123456@%s/test?sslmode=disable", hostAndPort)
-
-	log.Println("connecting to database on url:", databaseURL)
-
-	resource.Expire(120) // Tell docker to kill the container in 120 seconds.
-
-	// Exponential backoff-retry, because the application in the container might
-	// not be ready to accept connections yet.
-	pool.MaxWait = 120 * time.Second
-	if err = pool.Retry(func() error {
-		db, err = sql.Open("postgres", databaseURL)
-		if err != nil {
-			return err
-		}
-		return db.Ping()
-	}); err != nil {
-		log.Fatal("could not connect to docker:", err)
-	}
-
-	return func() {
-		if err := pool.Purge(resource); err != nil {
-			log.Fatal("could not purge resource:", err)
-		}
-	}
+	return err
 }
